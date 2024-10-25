8d8964a2287e4732721c84b5f80d99cfc9466b1b
ivysonin <ivysonthauan4@mail.com>
2024-10-25 20:47:15 -0300

    Simulando sites de pokédex

---
+++ pokedex.py
@@ -0,0 +1,21 @@
+# Adicionar os pokémons
+# Tipo do pokémon
+# Fraqueza do pokémon
+# Exibir uma lista
+
+pokedex = {}
+
+print ("====== POKÉDEX ======\n") # Organização
+
+qnt_pokemons = int(input ("=== Digite a quantidade de pokémons: "))
+
+for i in range(qnt_pokemons) :
+    nome = input ("Digite o nome do pokémon: ")
+    tipo = input ("Digite o tipo do pokémon: ")
+    fraaqueza = input ("Digite a fraqueza do pokémon: ")
+    pokedex.update({nome : {"Tipo" : tipo , "Fraqueza" : fraaqueza}})
+
+print ("\n===== Pokémons Registrado na Pokédex =====\n") # Organização
+
+for chave,valor in pokedex.items() :
+    print(f"{chave} - {valor}")
