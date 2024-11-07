0ab1533d8db04de044313f8f1b23dbd65be0b9d4
Ivyson Thauan <ivysonthauan17@gmail.com>
2024-11-07 06:19:31 -0300

    Selecionar ingredientes

--- pastelaria.py
+++ pastelaria.py
@@ -8,6 +8,7 @@ ingrediente5 = "carne"
 # Lista do que vai no pastel
 pastel = []
 
+# Organização
 print ("======== Bem-vindo a Pastelaria Nunes ========")
 print ("======== Aqui está nosso cardápio: \n")
 
@@ -19,43 +20,51 @@ print ("4- Pizza")
 print ("5- Carne")
 
 while True:
-    escolha = input ("Digite de 1 a 5 para escolha, ou digite 'sair' quando terminar de escolher: ".lower())
+    escolha = input ("Digite de 1 a 5 para escolha, ou digite 'sair' quando terminar de escolher: ").lower()
+    
     if escolha == "sair" :
         print ("Você terminou de escolher !\n")
         break
+        
     elif escolha == "1" :
-        if ingrediente1 not in pastel:
+        if ingrediente1 not in pastel: # Se não estiver, adiciono
             print ("Sabor adicionado, mais algum?\n")
             pastel.append(ingrediente1)
         else:
             print ("Esse sabor você já escolheu, escolha outro !\n")
+            
     elif escolha == "2" :
-        if ingrediente2 not in pastel:
+        if ingrediente2 not in pastel: # Se não estiver, adiciono
             print ("Sabor adicionado, mais algum?\n")
             pastel.append(ingrediente2)
         else:
             print ("Esse sabor você já escolheu, escolha outro !\n")
+            
     elif escolha == "3" :
-        if ingrediente3 not in pastel:
+        if ingrediente3 not in pastel: # Se não estiver, adiciono
             print ("Sabor adicionado, mais algum?\n")
             pastel.append(ingrediente3)
         else:
             print ("Esse sabor você já escolheu, escolha outro !\n")
+            
     elif escolha == "4" :
-        if ingrediente4 not in pastel:
+        if ingrediente4 not in pastel: # Se não estiver, adiciono
             print ("Sabor adicionado, mais algum?\n")
             pastel.append(ingrediente4)
         else:
             print ("Esse sabor você já escolheu, escolha outro !\n")
+            
     elif escolha == "5" :
-        if ingrediente5 not in pastel:
+        if ingrediente5 not in pastel: # Se não estiver, adiciono
             print ("Sabor adicionado, mais algum?\n")
             pastel.append(ingrediente5)
         else:
             print ("Esse sabor você já escolheu, escolha outro !\n")
+            
     else:
         print ("\nVocê está fazendo algo errado, tente de novo\n")
 
+# Organização
 print ("======== Esses são os ingredientes que vão no seu pastel: \n")
 for ingrediente in pastel:
     print (f"- {ingrediente}")
